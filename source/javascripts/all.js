487a65153fa152ea9bac9e4704656602234fd7d6
Jean-Edern Lorillon <trashjelo@gmail.com>
2015-07-12 20:26:14 +0200

    copy website site format

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,3 +1,4 @@
 //= require jquery
 //= require bootstrap-sprockets
+//= require modal.js
 //= require_tree .
---
+++ source/javascripts/modal.js
@@ -0,0 +1,12 @@
+// $('a').click(function (e) {
+//   e.stopPropagation();
+//   e.preventDefault();
+//   $('modal').modal('show');
+// })
+
+// tableau[1].getAttribute('href')
+
+$(document).ready(function(){
+  console.log("hello");
+  $('modal').modal('show');
+})
