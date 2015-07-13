69ddd569c960fd61ebd62606ffa1538215344501
Jean-Edern Lorillon <trashjelo@gmail.com>
2015-07-13 19:38:17 +0200

    wrote modal and continue changing <a> to modal trigger

--- source/javascripts/modal.js
+++ source/javascripts/modal.js
@@ -1,8 +1,8 @@
-// $('a').click(function (e) {
-//   e.stopPropagation();
-//   e.preventDefault();
-//   $('.modal').modal('show');
-// })
+$('a').click(function (e) {
+  e.preventDefault()
+  return false
+  // $('.modal').modal('show');
+})
 
 // tableau[1].getAttribute('href')
 
@@ -23,9 +23,9 @@
 
 $('#myModal').on('show.bs.modal', function (event) {
   var button = $(event.relatedTarget) // Button that triggered the modal
-  var recipient = button.data('href') // Extract info from data-* attributes
+  var href = button.data('href') // Extract info from data-* attributes
   // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
   // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-  var modal = $(this)
-  modal.find('.modal-link').attr("href", "http://www.google.com/");
+  var modal = $('#myModal')
+  modal.find('#modal-link').attr("href", href)
 })
